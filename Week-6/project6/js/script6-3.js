ebcdee20668105f306c3d20204e560e170aaadca
KarthikHanasiP <120626960+KarthikHanasiP@users.noreply.github.com>
2023-02-05 20:06:14 +0530

    Update script6-3.js

--- Week-6/project6/js/script6-3.js
+++ Week-6/project6/js/script6-3.js
@@ -1,19 +1,34 @@
 // Problem 6.3 Sort array of 0's,1's and 2's
-// TC => O(N^2)
+// TC => O(N)
 // SC => O(1)
-const sort = ((arr, n) => {
-    for(let i = 0; i < n; i++) {
-        for(let j = i+1; j < n; j++) {
-            if(arr[i] > arr[j]) {
-                [arr[i], arr[j]] = [arr[j], arr[i]];
-            }
-        }
+const sort = (arr, length) => {
+    let zeros = 0;
+    let ones = 0;
+    let twos = 0;
+    
+    //O(N)
+    for(let i of arr) {
+        if(i === 0) zeros++;
+        if(i === 1) ones++;
+        if(i === 2) twos++;
     }
 
-    console.log(arr);
-});
+    // //O(T1)
+    for(let i=0;i<zeros;i++) {
+        arr[i] = 0;
+    }
+    // //O(T2)
+    for(let i=zeros;i<(ones+zeros);i++) {
+        arr[i] = 1;
+    }
+    // //O(T3)
+    for(let i=(ones+zeros);i<(twos+ones+zeros);i++) {
+        arr[i] = 2;
+    }
+    return arr;
+}
 
-let arr= [0, 2, 1, 2, 0, 0];
+let arr= [0, 2, 1, 2, 0, 0,1];
 let arrLength = arr.length;
 
-sort(arr, arrLength);
+console.log(sort(arr, arrLength));
